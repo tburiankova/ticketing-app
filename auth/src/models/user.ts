c06fe3f18924bb2b72ba6610f7f824436089c6fd
Tereza Buriankova <tburiankova@gmail.com>
2020-10-07 22:19:22 +0200

    connected auth service to mongoose, created User model and  Password class with hashing function

---
+++ auth/src/models/user.ts
@@ -0,0 +1,47 @@
+import mongoose from 'mongoose';
+import { Password } from '../services/password';
+
+// interface describing properties to create a new User
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+// interface describing properties that a User model has
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+// interface describing properties that a User document has
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
+}
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+// do not declare cb as arrow fn - importance of "this" keyword to point to the user doc
+userSchema.pre('save', async function (done) {
+  if (this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'));
+    this.set('password', hashed);
+  }
+  done();
+});
+
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
+
+export { User };
