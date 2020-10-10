39a6eebc4e75466417b13c090a116bcc6f8d7952
Tereza Buriankova <tburiankova@gmail.com>
2020-10-10 14:54:42 +0200

    wrote tests for auth route handlers

---
+++ auth/src/test/setup.ts
@@ -0,0 +1,56 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import { app } from '../app';
+import request from 'supertest';
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signin(): Promise<string[]>;
+    }
+  }
+}
+
+let mongo: any;
+
+// hook functions
+// runs before all tests
+beforeAll(async () => {
+  process.env.JWT_KEY = '';
+  mongo = new MongoMemoryServer();
+  const mongoUri = await mongo.getUri();
+
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+});
+
+// reset all data between each test we run
+beforeEach(async () => {
+  const collections = await mongoose.connection.db.collections();
+
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});
+
+global.signin = async () => {
+  const email = 'test@test.com';
+  const password = 'password';
+
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({ email, password })
+    .expect(201);
+
+  // in testing env, cookie doesn't get attached -> we need to take it off manualy
+  const cookie = response.get('Set-Cookie');
+
+  return cookie;
+};
