7e83d1e9094da96451eb4fd33b3c5db087be03f9
Tereza Buriankova <tburiankova@gmail.com>
2020-10-12 17:21:38 +0200

    moved custom errors and middleware folder from the auth service to a common npm module

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,10 +1,9 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import jwt from 'jsonwebtoken';
+import { validateRequest, BadRequestError } from '@tbtickety/common';
 
-import { validateRequest } from '../middlewares/validate-requests';
 import { User } from '../models/user';
-import { BadRequestError } from '../errors/bad-request-error';
 import { Password } from '../services/password';
 
 const router = express.Router();
--- client/components/header.js
+++ client/components/header.js
@@ -18,7 +18,7 @@ export default ({ currentUser }) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <Link href="/">
-        <a className="navbar-brand">Tickity</a>
+        <a className="navbar-brand">Tickety</a>
       </Link>
       <div className="d-flex justify-content-end">
         <ul className="nav d-flex align-items-center">{links}</ul>
